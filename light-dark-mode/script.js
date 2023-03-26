f40681c7f4a8644d3b17bdb89b3a67ca71357f35
walwizard <goneisgone@hotmail.com>
2023-03-26 14:59:08 -0300

    JS 20 - Dark Mode , Toggle Theme With Javascript

---
+++ light-dark-mode/script.js
@@ -0,0 +1,15 @@
+const toggleSwitch = document.querySelector('input[type="checkbox"]');
+
+// Switch Thme Dynamically
+function switchTheme(event) {
+    // console.log(event.target.checked);
+    if(event.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark')
+    } else {
+        document.documentElement.setAttribute('data-theme', 'light')
+    }
+}
+
+
+// Event Listener
+toggleSwitch.addEventListener('change', switchTheme);
