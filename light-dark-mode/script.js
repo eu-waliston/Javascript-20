2a8069e0ba1a65eee573e1444b2b30a910e9cead
walwizard <goneisgone@hotmail.com>
2023-03-26 15:18:30 -0300

    JS 20 - Dark Mode , Toggle Theme With Javascript PT2

--- light-dark-mode/script.js
+++ light-dark-mode/script.js
@@ -1,12 +1,49 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 
+const nav = document.getElementById('nav');
+const toggleIcon = document.getElementById('toggle-icon');
+const image1 = document.getElementById('image1');
+const image2 = document.getElementById('image2');
+const image3 = document.getElementById('image3');
+const textBox = document.getElementById('text-box');
+
+
+// Dark or light Images
+function imageMode(color) {
+    image1.src = `img/undraw_proud_coder_${color}.svg`;
+    image2.src = `img/undraw_feeling_proud_${color}.svg`;
+    image3.src = `img/undraw_conceptual_idea_${color}.svg`;
+}
+
+
+//Dark mode style
+function darkMode(){
+    nav.style.backgroundColor = 'rgb(0 0 0 / 50%)'
+    textBox.style.backgroundColor = 'rgb(255 255 255 / 50%)';
+    toggleIcon.children[0].textContent = 'Dark Mode';
+    toggleIcon.children[1].classList.replace('fa-sun' , 'fa-moon');
+    imageMode('dark')
+}
+
+//Light mode style
+function lightMode(){
+    nav.style.backgroundColor = 'rgb(255 255 255 / 50%)'
+    textBox.style.backgroundColor = 'rgb(0 0 0 / 50%)';
+    toggleIcon.children[0].textContent = 'Light Mode';
+    toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun');
+    imageMode('light')
+}
+
+
 // Switch Thme Dynamically
 function switchTheme(event) {
     // console.log(event.target.checked);
     if(event.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark')
+        document.documentElement.setAttribute('data-theme', 'dark');
+        darkMode();
     } else {
         document.documentElement.setAttribute('data-theme', 'light')
+        lightMode();
     }
 }
 
