cc16e99765a7c61b77f80494775584e550207fb7
walwizard <goneisgone@hotmail.com>
2023-04-02 11:02:34 -0300

    JS - 20, Cleaning Up Out JS

--- Navigation-Nation/script.js
+++ Navigation-Nation/script.js
@@ -7,6 +7,17 @@ const nav3 = document.getElementById("nav-3");
 const nav4 = document.getElementById("nav-4");
 const nav5 = document.getElementById("nav-5");
 
+const navItens = [nav1, nav2, nav3, nav4, nav5];
+
+
+//Control Naviagtion Aniamtion
+function navAniamtion(direction1, direction2) {
+    navItens.forEach((nav, i) => {
+
+        nav.classList.replace(`slide-${direction1}-${i + 1}`, `slide-${direction2}-${i + 1}`)
+    });
+}
+
 function toggleNav() {
   //Toggle: Menu Bars Open/Close
   menuBars.classList.toggle("change");
@@ -15,52 +26,22 @@ function toggleNav() {
   overlay.classList.toggle("overlay-active");
   if (overlay.classList.contains("overlay-active")) {
     //Aniamte In overlay
-    overlay.classList.remove("overlay-slide-left");
-    overlay.classList.add("overlay-slide-right");
+    overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
 
     //Aniamte In - Nav Itens
-    nav1.classList.remove("slide-out-1");
-    nav1.classList.add("slide-in-1");
-
-    nav2.classList.remove("slide-out-2");
-    nav2.classList.add("slide-in-2");
-
-    nav3.classList.remove("slide-out-3");
-    nav3.classList.add("slide-in-3");
-
-    nav4.classList.remove("slide-out-4");
-    nav4.classList.add("slide-in-4");
-
-    nav5.classList.remove("slide-out-5");
-    nav5.classList.add("slide-in-5");
+    navAniamtion('out', 'in');
   } else {
+
     // animate out eoverlay
-    overlay.classList.remove("overlay-slide-right");
-    overlay.classList.add("overlay-slide-left");
+    overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
 
     //Aniamte Out - Nav Itens
-    nav1.classList.remove("slide-in-1");
-    nav1.classList.add("slide-out-1");
-
-    nav2.classList.remove("slide-in-2");
-    nav2.classList.add("slide-out-2");
-
-    nav3.classList.remove("slide-in-3");
-    nav3.classList.add("slide-out-3");
-
-    nav4.classList.remove("slide-in-4");
-    nav4.classList.add("slide-out-4");
-
-    nav5.classList.remove("slide-in-5");
-    nav5.classList.add("slide-out-5");
+    navAniamtion('in', 'out');
   }
 }
 
 //Event Listeners
 menuBars.addEventListener("click", toggleNav);
-
-nav1.addEventListener("click", toggleNav);
-nav2.addEventListener("click", toggleNav);
-nav3.addEventListener("click", toggleNav);
-nav4.addEventListener("click", toggleNav);
-nav5.addEventListener("click", toggleNav);
+navItems.forEach((nav) => {
+    nav.addEventListener('click', toggleNav);
+});
