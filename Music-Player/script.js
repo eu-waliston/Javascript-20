8a62efec4ce170957a79fac54ee797e1915a73de
walwizard <goneisgone@hotmail.com>
2023-04-23 11:15:37 -0300

    Music Player - JS to play audio

---
+++ Music-Player/script.js
@@ -0,0 +1,29 @@
+const music = document.querySelector("audio");
+const prevBtn = document.getElementById("prev");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
+
+//check if playing
+let isPlaying = false;
+
+// Play
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace('fa-play', 'fa-pause');
+  playBtn.setAttribute('title', 'Pause');
+  music.play();
+}
+
+//Pause
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace('fa-pause', 'fa-play');
+  playBtn.setAttribute('title', 'Play');
+
+  music.pause();
+}
+
+//Play or Pause
+playBtn.addEventListener("click", () => {
+  isPlaying ? pauseSong() : playSong();
+});
